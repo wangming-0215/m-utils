9c8c2bc89a8d83010f33917185d4b486034c1ed6
wangming <ming.wang@buttoninvest.com>
2019-02-13 17:18:37 +0800

    WIP

--- src/sleep.js
+++ src/sleep.js
@@ -2,6 +2,12 @@
  * 睡眠
  *
  * @param {number} ms 程序睡眠的时长,单位毫秒
+ *
+ * example:
+ * sleep(1000).then(() => doSomething());
+ * or:
+ * await sleep(1000);
+ * doSomethind();
  */
 export default function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
