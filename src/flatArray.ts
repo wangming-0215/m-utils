97b43689ce07b089025f9cc890a58829a7782b41
汪小明 <775899982@qq.com>
2020-05-30 14:04:54 +0800

    WIP

--- src/flatArray.ts
+++ src/flatArray.ts
@@ -1,17 +1,9 @@
 import isArray from './isArray';
 
-export default function flatArray(arr: any[]): any[] {
-  return arr.reduce((acc: any[], cur: any) => {
-    if (isArray(cur)) {
-      return acc.concat(flatArray(cur));
-    }
-    return acc.concat(cur);
+export default function flatArray<T = any>(arr: T[][]): T[] {
+  return arr.reduce((acc, cur) => {
+    return acc.concat(
+      isArray(cur) ? flatArray((cur as unknown) as T[][]) : cur
+    );
   }, []);
 }
-
-const arr = [
-  [1, 2, 3],
-  ['a', 'ab', ['e', 'f', 1, 3]]
-];
-
-console.log(flatArray(arr));
